36c728259c2da3aac1ad07ad99c87f7bb1873b4a
Dirk Eibach <dirk.eibach@cloos.de>
2019-08-26 16:21:53 +0200

    Inital commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Adapt subwcrev to git-svn
+
+If you are in the unfortunate situation that your build process depends on
+subwcrev (a Tortoise SVN commandline tool) and you want to use git-svn, this is
+the solution.
+Since subwcrev does not work on git-svn repositories, subwcrev.py gets the revision using
+the git commandline-tools. For the moment it only replaces $WCREV$, since this is all
+I need.
+If the git tools don't succeed, subwcrev.py falls back to the original subwcrev tool,
+so it can still be used on standard subversion repos.
+
+## Installation
+The original subwcrev has to be renamed to subwcrev_orig.exe.
+
+Build rubwcrev.exe with
+```
+C:\Python34\python.exe .\setup.py py2exe
+cp .\dist\* 'C:\Program Files\TortoiseSVN\bin\'
+```
+
+py2exe only works with python version <= 3.4
+Maybe there is a better replacement but I didn't care yet.
---
+++ subwcrev.py
@@ -0,0 +1,29 @@
+import argparse
+import subprocess
+import sys
+import re
+
+parser = argparse.ArgumentParser(description='Analyze batch files.')
+parser.add_argument('workdir',
+				   help='the workdir to analye')
+parser.add_argument('src',
+				   help='the source file')
+parser.add_argument('dest',
+				   help='the destination file')
+
+args = parser.parse_args()
+
+try:
+	label = subprocess.check_output("git -C " + args.workdir + " describe --always", shell=True).decode().strip()
+	revision = subprocess.check_output("git -C " + args.workdir + " svn find-rev " + label, shell=True).decode().strip()
+	print("Hash: " + label + " Revision: " + revision)
+	with open(args.src, "r") as src, open(args.dest, "w") as dest:
+		for line in src:
+			dest.write(re.sub("\$WCREV\$", label, line))
+
+except Exception as e:
+	print(e)
+	print("Falling back to original subwcrev")
+	cmdline = ["C:\\Program Files\\TortoiseSVN\\bin\\subwcrev_orig.exe"]
+	cmdline.extend(sys.argv[1:])
+	subprocess.call(cmdline, shell=True)
