fdf2ed9688adb7470e2548d67501d6b55bd009dd
Dirk Eibach <dirk.eibach@cloos.de>
2023-06-19 16:38:34 +0200

    Added dummy -f parameter and changed SVN version resolution

--- subwcrev.py
+++ subwcrev.py
@@ -10,17 +10,21 @@ parser.add_argument('src',
 				   help='the source file')
 parser.add_argument('dest',
 				   help='the destination file')
+parser.add_argument('-f',
+				   action='store_true',
+				   help='dummy only')
 
 args = parser.parse_args()
 
 try:
 #	label = subprocess.check_output("git -C " + args.workdir + " describe --always", shell=True).decode().strip()
-	label = subprocess.check_output("git -C " + args.workdir + " rev-parse --short HEAD", shell=True).decode().strip()
-	revision = subprocess.check_output("git -C " + args.workdir + " svn find-rev " + label, shell=True).decode().strip()
-	print("Hash: " + label + " Revision: " + revision)
+#	label = subprocess.check_output("git -C " + args.workdir + " rev-parse --short HEAD", shell=True).decode().strip()
+#	revision = subprocess.check_output("git -C " + args.workdir + " svn find-rev " + label, shell=True).decode().strip()
+#	print("Hash: " + label + " Revision: " + revision)
+	revision = subprocess.check_output("git svn -C " + args.workdir + " log --oneline --limit=1", shell=True).decode().split(' ', 1)[0][1:]
 	with open(args.src, "r") as src, open(args.dest, "w") as dest:
 		for line in src:
-			dest.write(re.sub("\$WCREV\$", label, line))
+			dest.write(re.sub("\$WCREV\$", revision, line))
 
 except Exception as e:
 	print(e)
